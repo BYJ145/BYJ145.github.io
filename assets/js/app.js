8cbb8637957cb6a7c5566c3d481632af7b643aea
Jose Gomez <GomezLopezJose145@gmail.com>
2021-06-24 18:32:59 -0500

    Se agrega elemento para desplazar pagina al inicio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,14 @@
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 500){
+        document.querySelector('.gotop').classList.add('show');
+    }else{
+        document.querySelector('.gotop').classList.remove('show');
+    }
+}
 
-
+document.querySelector('.gotop').addEventListener('click', () =>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
